499effa532f6eff3f5aad782f55d97f3978894e2
amir312 <57588920+amir312@users.noreply.github.com>
2019-12-14 16:36:15 +0200

    Add files via upload

---
+++ build/precache-manifest.db165280f0b7359ec956afa85a1968fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404e2e766319e913390081040f831e05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fb25bc231755d411d87",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4d5668b84111f90f8507",
+    "url": "/static/js/2.eccccaa7.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.eccccaa7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6fb25bc231755d411d87",
+    "url": "/static/js/main.e6e42fab.chunk.js"
+  },
+  {
+    "revision": "90f74c0edbbe30c70d34",
+    "url": "/static/js/runtime-main.e75aa327.js"
+  }
+]);
